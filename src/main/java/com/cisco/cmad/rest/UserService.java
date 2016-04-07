e970a12fce5f1f12ba9d0b39a20380244d940ac5
Narasimha <tynarasimhareddy@gmail.com>
2016-04-07 10:47:12 +0530

    Added thread local to maintain current DB session

--- src/main/java/com/cisco/cmad/rest/UserService.java
+++ src/main/java/com/cisco/cmad/rest/UserService.java
@@ -8,25 +8,22 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 
 
 @Path("/user")
 public class UserService {
 
-	static Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-	static ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-	static SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 	@GET
 	@Produces({MediaType.APPLICATION_JSON})
 	public List<Users> getUsers(){
-		Session session = sessionFactory.openSession();
-		List<Users> userList = session.createQuery("from Users").list();
-		session.close();
+		Session session = HibernateUtil.currentSession();
+		List<Users> userList = null;
+		try {
+			userList = session.createCriteria(Users.class).list();
+		} finally {
+			HibernateUtil.closeSession();
+		}
 		System.out.println("Total users are - "+userList.size());
 		for(Users user : userList)
 			System.out.println(user);
