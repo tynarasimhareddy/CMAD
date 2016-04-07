e39f53da45b997ec51a18b287fd88057aa458dcc
Narasimha <tynarasimhareddy@gmail.com>
2016-04-07 09:39:09 +0530

    Initial commit

---
+++ src/main/java/com/cisco/cmad/rest/UserService.java
@@ -0,0 +1,23 @@
+package com.cisco.cmad.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/user")
+public class UserService {
+
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public List<String> getUsers(){
+		List<String> users = new ArrayList<String>();
+		users.add("Narasimha Reddy");
+		users.add("NTY");
+		return users;
+	}
+}
