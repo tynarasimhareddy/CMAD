a80079c2e2ce8640528130dad8e88af4b9c7e117
Narasimha <tynarasimhareddy@gmail.com>
2016-04-07 10:37:50 +0530

    added hibernate integration

--- src/main/java/com/cisco/cmad/rest/UserService.java
+++ src/main/java/com/cisco/cmad/rest/UserService.java
@@ -1,6 +1,5 @@
 package com.cisco.cmad.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -8,17 +7,29 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
 
 @Path("/user")
 public class UserService {
 
+	static Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+	static ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+	static SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 	@GET
 	@Produces({MediaType.APPLICATION_JSON})
-	public List<String> getUsers(){
-		List<String> users = new ArrayList<String>();
-		users.add("Narasimha Reddy");
-		users.add("NTY");
-		users.add("Reddy");
-		return users;
+	public List<Users> getUsers(){
+		Session session = sessionFactory.openSession();
+		List<Users> userList = session.createQuery("from Users").list();
+		session.close();
+		System.out.println("Total users are - "+userList.size());
+		for(Users user : userList)
+			System.out.println(user);
+		return userList;
 	}
 }
